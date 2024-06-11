48fde3b782b530b7ddc0c488fbca9dec58ebdbcd
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-06-11 11:17:28 +0800

    refactor: 优化标准响应构造函数
    1、私有化响应格式的构造函数；
    2、明确标准响应格式；
    3、增加对可选字段的断言；
    4、更正内部命名冲突（不影响实际逻辑）。

--- commons/response.py
+++ commons/response.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response
 
 
 class Errcode(IntegerChoices):
+    """
+    项目内部定义的错误代码，用于前端识别 API 响应内容。
+    """
+
     # 通用错误码
     CONTINUE = 2, 'Continue'
     SUCCESS = 1, 'Success'
@@ -32,7 +36,7 @@ def wrap200(response: Response, code: Errcode) -> Response:
     :return: 符合标准格式的响应。
     """
     if not isinstance(response.data, dict) or len({'code', 'message', 'data'} - set(response.data.keys())) > 0:
-        response.data = resp(response.data, code=code)
+        response.data = _resp(response.data, code=code)
 
     return response
 
@@ -41,34 +45,56 @@ def resp200(data: Any = None, code: Errcode = Errcode.FAIL, msg: str = None, **k
     """
     构造标准格式的响应。
 
-    :param data: 要返回的数据，默认为空。
-    :param code: 自定义响应码，默认为失败。
-    :param msg: 响应消息。不提供则默认为 ``code`` 的标签。
+    标准响应格式为一个JSON对象，必定包含以下字段：
+
+    - ``code`` 表示错误代码，必定为 integer（int32）。
+    - ``message`` 表示错误提示，必定为 string 且不为空字符串，不应存在换行符，不建议以句号结尾。
+    - ``data`` 表示业务数据，必定存在，但可以为 ``null`` 或其它任意类型。
+
+    可能包含以下字段：
+
+    - ``prev`` 表示上一页的单个请求 URL，必定为 string 且不为空字符串。
+    - ``next`` 表示下一页的单个请求 URL，必定为 string 且不为空字符串。
+    - ``pages`` 表示总页数，必定为 integer（int64）。
+    - ``error`` 表示需要提供给外部的内部错误，一般为序列化器校验器产生的嵌套数组或嵌套对象，但也可以是其它任意类型。
+
+    :param data: 要返回的业务数据部分，默认为空。
+    :param code: 错误代码，默认为失败。
+    :param msg: 错误提示。不提供则默认为 ``code`` 的标签。
     :return: DRF 的响应类对象。
     """
-    data = resp(data, code, msg, **kwargs)
-    return Response(data)
+    body = _resp(data, code, msg, **kwargs)
+    return Response(body)
 
 
-def resp(data: Any = None, code: Errcode = Errcode.FAIL, msg: str = None, **kwargs: Any) -> dict:
+def _resp(data: Any = None, code: Errcode = Errcode.FAIL, msg: str = None, **kwargs: Any) -> dict:
     """
-    构造标准响应格式。
+    构造标准的响应数据格式。
 
-    :param data: 要返回的数据，默认为空。
-    :param code: 自定义响应码，默认为失败。
-    :param msg: 响应消息。不提供则默认为 ``code`` 的标签。
+    :param data: 要返回的业务数据部分，默认为空。
+    :param code: 错误代码，默认为失败。
+    :param msg: 错误提示。不提供则默认为 ``code`` 的标签。
     :return: 一个字典。
     """
-    data = {
+    body = {
         'code': code.value,
         'message': msg or code.label,
         'data': data,
         **kwargs,
     }
-    if 'error' in data and data['error'] is None:
-        _ = data.pop('error')
+    if 'error' in body and body['error'] is None:
+        _ = body.pop('error')
 
-    assert 'data' in data
-    assert 'code' in data and type(data['code']) is int
-    assert 'message' in data and type(data['message']) is str
-    return data
+    assert 'data' in body, 'API响应缺少 "data" 字段。'
+    assert 'code' in body, 'API响应缺少 "code" 字段。'
+    assert 'message' in body, 'API响应缺少 "message" 字段。'
+    assert type(body['code']) is int, 'API响应的 "code" 字段必须是 int 类型。'
+    assert type(body['message']) is str, 'API响应的 "message" 字段必须是 str 类型。'
+    assert body['message'], 'API响应的 "message" 字段不允许为空字符串。'
+    if 'prev' in body:
+        assert type(body['prev']) is str and body['prev'], 'API响应的 "prev" 字段必须是 str 类型并且不允许为空字符串。'
+    if 'next' in body:
+        assert type(body['next']) is str and body['next'], 'API响应的 "next" 字段必须是 str 类型并且不允许为空字符串。'
+    if 'pages' in body:
+        assert type(body['pages']) is int, 'API响应的 "pages" 字段必须是 int 类型。'
+    return body
