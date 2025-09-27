8083e5dac187dae8067209dd74d5202b558d3a55
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2025-09-27 09:55:43 +0800

    style: 规范 settings.TEMPLATES 的代码

--- django_template_repo/settings.py
+++ django_template_repo/settings.py
@@ -58,14 +58,14 @@ TEMPLATES = [
             PROJECT_ROOT / 'templates',
         ],
         APP_DIRS=True,
-        OPTIONS={
-            'context_processors': [
+        OPTIONS=dict(
+            context_processors=[
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-        },
+        ),
     ),
 ]
 
