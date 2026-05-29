e0cf7833b6ef97b328274e9c1c34d9e5d68f2cde
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-05-29 12:11:06 +0800

    refactor(utils): 完善 Cacher 对下标的校验和解析

--- utils/cache.py
+++ utils/cache.py
@@ -15,18 +15,27 @@ class Cacher:
     def __contains__(self, key: str) -> bool:
         return self.target.has_key(key)
 
-    def __getitem__(self, key: str) -> Any:
-        return self.target.get(key)
-
-    def __setitem__(self, key: tuple[str, slice | int], value) -> None:
-        name, timeout = key
-        match timeout:
-            case int():
-                self.target.set(name, value, timeout=timeout)
-            case slice():
-                self.target.set(name, value, timeout=timeout.start * 3600 + timeout.stop * 60 + timeout.step)
+    def __getitem__(self, item: str | tuple[str, Any]) -> Any:
+        match item:
+            case str(key):
+                return self.target.get(key)
+            case [str(key), default]:
+                return self.target.get(key, default=default)
             case _:
-                raise TypeError(f'Cacher 不接受 {type(timeout).__name__} 类型的 timeout。')
+                raise TypeError('无法解析 Cacher.__getitem__() 的参数。')
+
+    def __setitem__(self, item: tuple[str, slice | int], value) -> None:
+        match item:
+            case [str(key), int(timeout)]:
+                self.target.set(key, value, timeout=timeout)
+            case [str(key), slice() as t]:
+                self.target.set(key, value, timeout=(t.start or 0) * 3600 + (t.stop or 0) * 60 + (t.step or 0))
+            case [str(), t]:
+                raise TypeError(f'Cacher.__setitem__() 不接受 {type(t).__name__} 类型的 timeout 值。')
+            case [key, _]:
+                raise TypeError(f'Cacher.__setitem__() 不接受 {type(key).__name__} 类型的 key 值。')
+            case _:
+                raise TypeError('无法解析 Cacher.__setitem__() 的参数。')
 
     def __delitem__(self, key: str) -> None:
         self.target.delete(key)
